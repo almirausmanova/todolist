b29bbc644e97acd3cf791f5ee1f25a38b16b1587
almirausmanova <almira.usmanova24@gmail.com>
2023-01-17 19:59:35 +0500

    first commit

---
+++ src/todosData.js
@@ -0,0 +1,56 @@
+const todosData = [
+    
+    {
+        id: 0,
+        description: 'Гигиенические процедуры утром',
+        completed : false
+    },
+    {
+        id: 1,
+        description: 'Завтрак',
+        completed : false
+    },
+    {
+        id: 2,
+        description: 'Работа по дому.Программирование',
+        completed : false
+    },
+    {
+        id: 3,
+        description: 'Занятия спортивными гирями.Программирование',
+        completed : false
+    },
+    {
+        id: 4,
+        description: 'Обед',
+        completed : false
+    },
+    {
+        id: 5,
+        description: 'Поход в магазин',
+        completed : false
+    },
+    {
+        id: 6,
+        description: 'Скандинавская ходьба',
+        completed : false
+    },
+    {
+        id: 7,
+        description: 'Программирование.Позвонить маме',
+        completed : false
+    },
+    {
+        id: 8,
+        description: 'Ужин.Программирование',
+        completed : false
+    },
+    {
+        id: 9,
+        description: 'Изучение английского языка',
+        completed : false
+    }
+];
+
+
+export default todosData;
